288be993b04d432b2f31d6cd0886e585798c67f1
echolimitless <echolimitless@outlook.com>
2023-03-30 10:31:34 +0900

    課題３（非同期処理のテスト）完了

--- WorkSpace/AsyncAwait/AsyncAwait.Tests/FilesTests.cs
+++ WorkSpace/AsyncAwait/AsyncAwait.Tests/FilesTests.cs
@@ -1,10 +1,11 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace AsyncAwait.Tests
 {
-    public class FilesTests
+    public class FilesTests : IDisposable
     {
         private const string ExistFileName = "test.txt";
         private const string TextFileContent = "Hello, xUnit.net!";
@@ -16,6 +17,25 @@ namespace AsyncAwait.Tests
                 File.Delete(ExistFileName);
 
             File.WriteAllText(ExistFileName, TextFileContent);
+        }
+
+        public void Dispose()
+        {
+            if (File.Exists(ExistFileName))
+                File.Delete(ExistFileName);
+        }
+
+        [Fact]
+        public async Task ReadAllTextAsyncWhenExistFile()
+        {
+            Assert.Equal(TextFileContent, await Files.ReadAllTextAsync(ExistFileName));
+        }
+
+        [Fact]
+        public async Task ReadAllTextAsyncWhenNotExistFile()
+        {
+            await Assert.ThrowsAsync<FileNotFoundException>(
+                () => Files.ReadAllTextAsync(NotExistFileName));
         }
     }
 }
